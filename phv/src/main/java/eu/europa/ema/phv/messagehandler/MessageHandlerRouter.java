53069eecd26530b138c7b218631bcb73b726696e
Mirko Bernardoni <mirko.bernardoni@gmail.com>
2014-06-12 16:51:10 +0100

    Spring draft configuration

--- phv/src/main/java/eu/europa/ema/phv/messagehandler/MessageHandlerRouter.java
+++ phv/src/main/java/eu/europa/ema/phv/messagehandler/MessageHandlerRouter.java
@@ -4,7 +4,6 @@
 package eu.europa.ema.phv.messagehandler;
 
 import org.apache.camel.spring.SpringRouteBuilder;
-
 import eu.europa.ema.phv.common.exception.UnexpectedResultException;
 
 /**
@@ -16,6 +15,13 @@ import eu.europa.ema.phv.common.exception.UnexpectedResultException;
  * @revisionDate $Date: 2003/12/19 10:51:34 12 Jun 2014 $
  */
 public class MessageHandlerRouter extends SpringRouteBuilder {
+    private static final String AXWAY_EP = "jms:queue:jms/axway?concurrentConsumers=20&asyncConsumer=true";
+    private static final String VALID_EP ="direct:valid";
+    private static final String INVALID_EP ="direct:invalid";
+    private static final String OUTBOUND_EP = "jms:queue:/jms/outbound";
+    private static final String MESSAGE_STORE_EP="direct:messageStore";
+    private static final String CONTINUE_ROUTING_EP="direct:continueRouting";
+    private static final String ADR_PARSER_HUMAN_EP="jms:queue:jms/parser/human/adr";
 
     /*
      * (non-Javadoc)
@@ -25,14 +31,14 @@ public class MessageHandlerRouter extends SpringRouteBuilder {
     @Override
     public void configure() throws Exception {
         //@formatter:off
-        from("jms:queue:jms/queue0?concurrentConsumers=20&asyncConsumer=true")
+        from(AXWAY_EP)
             .transacted()
             .processRef("XmlValidator")
             .choice()
                 .when(header("ValidationResult").isEqualTo("valid"))
-                    .to("direct:valid")
+                    .to(VALID_EP)
                  .when(header("ValidationResult").isEqualTo("invalid"))
-                     .to("direct:invalid")
+                     .to(INVALID_EP)
                  .otherwise()
                      .throwException(new UnexpectedResultException("Validation didn't set the right header"));
         //@formatter:on
@@ -42,40 +48,40 @@ public class MessageHandlerRouter extends SpringRouteBuilder {
 
     private void invalidXmlBranch() {
         //@formatter:off        
-        from("direct:invalid")
+        from(INVALID_EP)
             .transacted()
             .processRef("MetadataExtractor")
             .to("direct:messageStore")
             .beanRef("AckTranslator")
-        .to("jms:queue:jms/ack");
+        .to(OUTBOUND_EP);
         //@formatter:on        
     }
 
     private void validXmlBranch() {
         //@formatter:off        
-        from("direct:valid")
+        from(VALID_EP)
             .transacted()
             .beanRef("MetadataEnricher")            
             .multicast()
-            .to("direct:messageStore")
-            .to("direct:notWebtrader");
+                .to(MESSAGE_STORE_EP)
+                .to(CONTINUE_ROUTING_EP);
         //@formatter:on
         
         //@formatter:off        
-        from("direct:messageStore")
+        from(MESSAGE_STORE_EP)
             .beanRef("StoreEnricher");
         // @formatter:on
         // TODO: Call the persistence layer here
         
         //@formatter:off        
-        from("direct:notWebtrader")
+        from(CONTINUE_ROUTING_EP)
              .filter(header("foo").isEqualTo("bar")) // TODO: Filter by NOT Webtrader
              .choice()
-                .when(header("foo").isEqualTo("bar"))
-                    .to("jms:queue:/jms/icsrEma")
-                .when(header("foo").isEqualTo("cheese"))
+                .when(header("foo").isEqualTo("bar")) // TODO: is Icsr to EMA
+                    .to(ADR_PARSER_HUMAN_EP)
+                .when(header("foo").isEqualTo("cheese")) // TODO: is Rerouting
                     .beanRef("RoutingTranslator")
-                    .to("jms:queue:/jms/outbound")
+                    .to(OUTBOUND_EP)
                 .otherwise()
                     .throwException(new UnexpectedResultException("Message not recognized by the router"));
         // @formatter:on
